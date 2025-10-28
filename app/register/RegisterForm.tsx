3968f199ced47a907b4ecd17394ef4a95776d935
David H <davidweh7878@hotmail.com>
2025-10-28 15:17:34 -0500

    feat: mantine setup

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -1,10 +1,24 @@
+import { Button, PasswordInput, TextInput } from '@mantine/core';
 import { Form } from 'react-router';
 import TextField from '~/components/ui/TextField';
 
 export default function RegisterForm() {
   return (
     <Form className="w-full space-y-4" method="post">
-      <TextField
+      <TextInput
+        label="Your name"
+        placeholder="Your full name here"
+        name="name"
+        withAsterisk
+      />
+      <TextInput
+        label="Email"
+        placeholder="me@example.com"
+        name="email"
+        withAsterisk
+      />
+      <PasswordInput label="Password" name="password" withAsterisk />
+      {/* <TextField
         label="Your Name"
         labelProps={{ htmlFor: 'name' }}
         inputProps={{ name: 'name' }}
@@ -18,14 +32,14 @@ export default function RegisterForm() {
         label="Password"
         labelProps={{ htmlFor: 'password' }}
         inputProps={{ name: 'password', type: 'password' }}
-      />
+      /> */}
       <br />
-      <button
+      <Button
         type="submit"
         className="w-full flex font-semibold text-sm p-2 items-center justify-center text-center rounded-sm bg-blue-600"
       >
         Submit
-      </button>
+      </Button>
     </Form>
   );
 }
